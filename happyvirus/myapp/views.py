9a5cb97ab13091c40b0a0938d72972b07fd87a59
eeyeji <dldpwl026@gmail.com>
2023-01-30 16:12:39 +0900

    장고 테스트
    - 테스트중입니다.

--- happyvirus/myapp/views.py
+++ happyvirus/myapp/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
-
+# from django.shortcuts import render
+from django.http import HttpResponse
 # Create your views here.
+def index(request):
+    return HttpResponse("안녕하세요 happyvirus에 오신 것을 환영합니다.")

00d430bb9ad271c8eafbe48ae96b2e5ce6b80f53
eeyeji <dldpwl026@gmail.com>
2023-01-30 17:35:47 +0900

    수정

--- happyvirus/myapp/urls.py
+++ happyvirus/myapp/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 
 from . import views
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+

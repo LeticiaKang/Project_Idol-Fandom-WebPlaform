eee18c0cff00f187c4259d44b0dce8fcac8b21ce
eeyeji <dldpwl026@gmail.com>
2023-01-30 22:01:25 +0900

    웹구현
    수정

--- happyvirus/myapp/urls.py
+++ happyvirus/myapp/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-
 from . import views
 
-from django.conf.urls.static import static
-from django.conf import settings
-
 urlpatterns = [
-    path('', views.index, name='index'),
+    path("", views.index, name='index'),
+    path('join', views.register, name='join'),
+    path('login', views.login, name='login'),
+    path('service', views.service, name='service'),
+    path('contact', views.contact, name='contact'),
 ]
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
 

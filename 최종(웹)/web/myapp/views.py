d1a9436f1a3b37087b28907e8eca04f96ab577f1
eeyeji <dldpwl026@gmail.com>
2023-02-10 15:10:04 +0900

    웹구현

--- 최종(웹)/web/myapp/views.py
+++ 최종(웹)/web/myapp/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render #함수를 실행하면 html이 실행됨
+from django.shortcuts import render, redirect, get_object_or_404 #함수를 실행하면 html이 실행됨
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import redirect
 from .models import User #입력받은 회원정보로 회원가입을 하기 위해서 import 시켜줌
+
+from django.contrib.auth.hashers import check_password #비밀번호 변경하기 위해서 import 시켜줌 
+from django.views.decorators.http import require_GET, require_POST
 
 from myapp.models import Image
 from myapp.form import ImageUpload
@@ -11,6 +14,9 @@ from myapp.Route import route
 from glob import glob
 from PIL import Image as pil_img
 from myapp.feature_extractor import FeatureExtractor
+
+from .models import Posting, Comment
+from .forms import PostingModelForm, CommentModelForm
 
 def index(request):
     return render(request, 'index.html')
@@ -211,3 +217,69 @@ def FindSimilarPicture(img, name:str):
     ids = np.argsort(dists)  # np.argsort는 배열안의 숫자를 오름차순해서 인덱스로 표현 해준다. 상위 30개만 ids에 다시 저장
     scores = [(dists[id], img_paths[id]) for id in ids]    # 그래서 for문으로 dists[id]에 넣으면 인덱스 역할을 해서 순서대로 뽑힘.
     return scores
+
+
+
+@require_GET
+def feed(request):
+    postings = Posting.objects.all()
+    return render(request, 'feed.html', {
+        'postings': postings,
+    })
+
+
+
+
+
+@require_GET
+def posting_list(request):
+    postings = Posting.objects.all()
+    return render(request, 'posting_list.html', {
+        'postings': postings,
+    })
+
+@require_GET
+def posting_detail(request, posting_id):
+    posting = get_object_or_404(Posting, id=posting_id)
+    comments = posting.comments.all()  # posting.comment_set 이 아닌 이유는 Model => related_name 때문
+    return render(request, 'posting_detail.html', {
+            'posting': posting,
+            'comments': comments,
+    })
+
+
+@require_POST
+def create_posting(request):
+    form = PostingModelForm(request.POST, request.FILES)  # 데이터와 파일을 검증하고 저장할 준비를 한겨
+    if form.is_valid():  # 검증!
+        posting = form.save()  # 저장 => Posting 객체를 return
+        return redirect(posting_list)  # 성공하면 detail page
+    else:
+        return redirect('posting_list')  #실패하면 list page
+
+
+@require_POST
+def delete_posting(request, posting_id):
+    posting = get_object_or_404(Posting, id=posting_id)
+    posting.delete()
+    return redirect('posting_list')
+
+
+@require_POST
+def create_comment(request, posting_id):
+    posting = get_object_or_404(Posting, id=posting_id)
+    form = CommentModelForm(request.POST)  # 가져올때 model에 의해 content를 가져오게 돼있음.
+    if form.is_valid():  # content값만 검증함
+        comment = form.save(commit=False)  # 아직 posting_id 가 비어있기 때문에, 저장하는 척 만 하고 Comment 객체를 return 한다.
+        comment.posting_id = posting.id  # 그러고 나서 commnet_id값을 직접 넣어준다.
+        # 이건 comment.posting = posting 하면 id가 알아서 들어가기 때문에 윗줄과 아예 똑같은 코드이다.
+        comment.save()
+    return redirect(posting)
+
+
+@require_POST
+def delete_comment(request, posting_id, comment_id):
+    posting = get_object_or_404(Posting, id=posting_id)
+    comment = get_object_or_404(Comment, id=comment_id, posting_id=posting_id)
+    comment.delete()
+    return redirect(posting)

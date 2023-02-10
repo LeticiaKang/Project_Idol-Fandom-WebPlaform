eb11445d912b76967c50345b52e3a22d0d8761e8
eeyeji <dldpwl026@gmail.com>
2023-02-10 17:35:29 +0900

    웹구현(최종)
    -정국 사진 추가

--- 최종(웹)/web/myapp/offline.py
+++ 최종(웹)/web/myapp/offline.py
@@ -7,14 +7,15 @@ if __name__ == '__main__':
     fe = FeatureExtractor()
 
     # Iterate through images (Change the path based on your image location)
-    for img_path in sorted(Path("./static/img").glob("*.jpg")):
+    for img_path in sorted(Path("C:/Users/user/Desktop/프로젝트/HappyVirus/최종(웹)/web/myapp/static/img/뷔").glob("*.jpg")):
         print(img_path)  # e.g., ./static/img/xxx.jpg
         
         # feature extration
         feature = fe.extract(img=Image.open(img_path))
         
         # Save the Numpy array (.npy) on designated path
-        feature_path = Path("./static/feature") / (img_path.stem + ".npy")  # e.g., ./static/feature/xxx.npy
+        feature_path = Path("C:/Users/user/Desktop/프로젝트/HappyVirus/최종(웹)/web/myapp/static/feature/뷔") / (img_path.stem + ".npy")  # e.g., ./static/feature/xxx.npy
         print(feature_path)
         
         np.save(feature_path, feature)
+        #C:/Users/user/Desktop/프로젝트/HappyVirus/최종(웹)/web/myapp/offline.py
